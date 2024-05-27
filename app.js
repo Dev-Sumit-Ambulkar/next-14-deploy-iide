63ab6d006a35e69687661448824486c43554cedf
Sumit <sumit.finoux@blinkx.in>
2024-05-27 19:02:03 +0530

    Added route / home

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/",(req,res)=>{
+    const mes = {
+        "status":200
+    }
+    res.send(mes);
+})
+
 app.route("/api/auth/signup")
 .post(async(req,res)=>{
     try{
